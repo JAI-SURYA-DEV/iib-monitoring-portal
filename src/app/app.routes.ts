eb2562bf83fa6bfd91e91f90546cf0f420fac70e
JAI SURYA <jaisuryadeveloper@gmail.com>
2026-06-15 14:50:49 +0530

    page routes are updated with child in mainlayout component updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { AnalyticsComponent } from './features/analytics/analytics.component';
+import { AuditComponent } from './features/audit/audit.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { MonitoringComponent } from './features/monitoring/monitoring.component';
+import { ReportsComponent } from './features/reports/reports.component';
+import { ValidationComponent } from './features/validation/validation.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'reports',
+        component: ReportsComponent
+      },
+      {
+        path: 'analytics',
+        component: AnalyticsComponent
+      },
+      {
+        path: 'monitoring',
+        component: MonitoringComponent
+      },
+      {
+        path: 'validation',
+        component: ValidationComponent
+      },
+      {
+        path: 'audit',
+        component: AuditComponent
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
